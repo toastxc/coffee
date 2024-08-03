e9ea04abf5ca859f352172dfc5882a21f426ce65
ToastXC <100072983+toastxc@users.noreply.github.com>
2024-08-03 11:50:04 +0800

    added more milk options

--- crates/coffee-front/src/routes/mod.rs
+++ crates/coffee-front/src/routes/mod.rs
@@ -3,5 +3,5 @@ pub mod home;
 pub mod order;
 
 pub const COFFEE_TYPE: [&str; 3] = ["Flat white", "Latte", "Mocha"];
-pub const MILK_TYPE: [&str; 5] = ["Cows", "A2", "Almond", "Soy", "None"];
+pub const MILK_TYPE: [&str; 7] = ["Cows", "A2", "Almond", "Soy",  "Oat", "Lactose Free", "None"];
 pub const TEMP_TYPE: [&str; 5] = ["Iced", "Meh", "Warm", "Hot", "Extra hot"];
